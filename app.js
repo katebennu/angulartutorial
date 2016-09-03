d12dd2df263315124bfbfcea500ac5fa95061808
katebennu <kate.dorrer@gmail.com>
2016-09-03 06:32:30 +0300

    level 1

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-(function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+(function() {
   var app = angular.module('gemStore', []);
-  app.controller("StoreController", function(){
-    this.product = gem;
+
+  app.controller('StoreController', function(){
+    this.products = gems;
   });
 
+  var gems = [
+  {
+    name: 'Azurite',
+    price: 110.50,
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name: 'Pentagonal Gem',
+    price: 155.00,
+    canPurchase: true,
+    soldOut: false
+  }
+  ];
 })();
